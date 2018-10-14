b0c6957f7b69469fc5390187e554c34351f66208
TRIF3X <thewizardofcrypto@gmail.com>
2018-10-14 16:16:05 -0500

    first initial commit

---
+++ src/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.css';
+
+class Header extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = this.state
+    }
+    render() {
+        return (
+            <div className='headerDiv'>
+                <h2 className='myName'>Conner Hoessly</h2>
+                <div className='headerLinks'>
+                    <a href='#' className='links'>About me</a>
+                    <a href='#' className='links'>Projects</a>
+                    <a href='#' className='links'>Contact me</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
