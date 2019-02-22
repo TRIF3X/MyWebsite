0b7063b97fe1efb8b92cd713e3f6271a2f162fca
TRIF3X <thewizardofcrypto@gmail.com>
2019-02-22 14:09:53 -0600

    update to design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import { Button, Icon } from 'semantic-ui-react'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
         <div className='mainDiv'>
-          Conner Hoessly<br/>
-          _____
+          <div>
+            Conner Hoessly
+          </div>
+          <div>______</div>
+          <div>
+            <Button basic inverted>
+            Projects
+            </Button>
+            <Button basic inverted>
+            About
+            </Button>
+            <Button basic inverted>
+            Resume
+            </Button>
+          </div>
+          <div>
+            <Icon circular className='black github' size='tiny'/>
+            <Icon circular className='black linkedin' size='tiny'/>
+            <Icon circular className='black mail' size='tiny'/>
+          </div>
         </div>
     );
   }
