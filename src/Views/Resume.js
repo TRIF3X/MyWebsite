e0771d2c4e1b4f3169400e5a7269dafce0db1a3c
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-04 11:10:03 -0600

    push for test of resume

--- src/Views/Resume.js
+++ src/Views/Resume.js
@@ -2,21 +2,27 @@ import React from 'react'
 import Iframe from 'react-iframe'
 import { Button, Modal } from 'semantic-ui-react'
 
+let width = window.innerWidth;
+
 const Resume = () => (
+
+
   <Modal
     trigger={
     <Button basic inverted className='mainButtonActual'>
     Resume
     </Button>
     }>
+    {width < 600  ? 
+    <div>
+      View my hosted resume <a href="https://resume.creddle.io/embed/bgshrnisard" target="_blank" rel="noopener noreferrer">here</a>
+    </div>:
     <Iframe 
     url = "https://resume.creddle.io/embed/bgshrnisard"
     width="100%"
     height="90vh"
-    display="initial"
-    allowFullScreen
     />
-
+  }
   </Modal>
 )
 
