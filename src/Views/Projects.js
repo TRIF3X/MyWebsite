020b27b9d7fbf63068aa5f1586309ee9a1d71f8a
TRIF3X <thewizardofcrypto@gmail.com>
2019-03-31 09:10:36 -0500

    added weatherAPI to priejcts

--- src/Views/Projects.js
+++ src/Views/Projects.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Backpaca from '../Image/backpaca_home.PNG'
+import WeatherAPI from '../Image/weatherAPI.PNG'
 import { Button, Modal, Image } from 'semantic-ui-react'
 
 const Projects = () => (
@@ -8,6 +9,7 @@ const Projects = () => (
     Projects
     </Button>
   }>
+  {/* Backpaca */}
   <Modal.Header>Backpaca</Modal.Header>
   <Modal.Content image>
     <Image wrapped size='big' src={Backpaca} onClick={() => window.open("https://backpaca.app/", "_blank")} />
@@ -16,6 +18,17 @@ const Projects = () => (
         <h5>
           Front-End: React and Apollo<br/>
           Back-End: GraphQL Yoga and Prisma 
+        </h5>
+    </Modal.Description>
+  </Modal.Content>
+  {/* WeatherAPI */}
+  <Modal.Header>WeatherAPI</Modal.Header>
+  <Modal.Content image>
+    <Image wrapped size='medium' src={WeatherAPI} onClick={() => window.open("https://gallant-albattani-4f5744.netlify.com/", "_blank")} />
+    <Modal.Description>
+      <p>WeatherAPI is a simple app that allows a user to enter their zip code and receive the current weather along with a few other details.</p>
+        <h5>
+          Front-End: React, Axios, Semantic-ui<br/>
         </h5>
     </Modal.Description>
   </Modal.Content>
